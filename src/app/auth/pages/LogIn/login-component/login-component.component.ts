1e409f20ae5b9e8966eee94b699982aa66168bcc
Gothmaur <78580948+Gothmaur@users.noreply.github.com>
2024-01-15 00:29:10 -0600

    Se aplica el registro de usuarios desde login

--- src/app/auth/pages/LogIn/login-component/login-component.component.ts
+++ src/app/auth/pages/LogIn/login-component/login-component.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { AuthService } from 'src/app/auth/Services/auth.service';
+import { RegisterComponentComponent } from '../../registro/register-component/register-component.component';
+import { UserService } from 'src/app/dashboard/pages/users/services/user.service';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-login-component',
@@ -22,7 +25,9 @@ export class LoginComponentComponent {
     password: this.passControl
   });
 
-  constructor(private authService: AuthService){}
+  constructor(private matDialog: MatDialog, 
+    private authService: AuthService,
+    private userServices: UserService){}
 
   login():void{
     if(this.loginForm.invalid){
@@ -30,5 +35,34 @@ export class LoginComponentComponent {
     }else{
       this.authService.login(this.loginForm.getRawValue());
     }
+  }
+
+  Registrarse():void{
+    //Abre form
+    const dialogRef=this.matDialog.open(RegisterComponentComponent)
+    //Cuando cierre, haré esto
+    .afterClosed().subscribe({
+      next: (v) => {
+        //Log para ver que retorna el form
+        //console.log(v);
+        if(v) {
+          //console.log('recibí el valor ',v);
+          this.userServices.createUser({
+            nombre: v.nombre,
+            apellido1: v.apellido1,
+            apellido2: v.apellido2,
+            telefono: v.telefono,
+            direccion: v.direccion,
+            email: v.email,
+            clave: v.clave,
+            tipo: v.tipo
+          });
+          //console.log('Aprobado');
+        }
+        else{ 
+          //console.log('cancelado');
+        }
+      },
+    });
   }
 }
--- src/app/auth/pages/registro/register-component/register-component.component.ts
+++ src/app/auth/pages/registro/register-component/register-component.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { MatDialogRef } from '@angular/material/dialog';
+import { Store } from '@ngrx/store';
+import { selectAuthRole } from 'src/app/store/auth/auth.selector';
 
 @Component({
   selector: 'app-register-component',
@@ -6,5 +10,65 @@ import { Component } from '@angular/core';
   styleUrls: ['./register-component.component.scss']
 })
 export class RegisterComponentComponent {
+  rol:string | undefined;
+  
+  //Validaciones requeridas para cada uno de los campos 
+  NombreControl = new FormControl<string | null>(null,[
+    Validators.required, //Requerido
+    Validators.minLength(2) //Tamaño minimo de 2 caracteres
+  ]);
+  PrimerApellidoControl = new FormControl<string | null>(null,[
+    Validators.required, //Requerido
+    Validators.minLength(2) //Tamaño minimo de 2 caracteres
+  ]);
+  SegundoApellidoControl = new FormControl<string | null>(null,[
+    Validators.minLength(2) //Tamaño minimo de 2 caracteres
+  ]);
+  TelefonoControl = new FormControl<string | null>(null,[
+    Validators.required, //Requerido
+    Validators.minLength(2) //Tamaño minimo de 2 caracteres
+  ]);
+  DireccionControl = new FormControl<string | null>(null,[
+    Validators.required, //Requerido
+    Validators.minLength(2) //Tamaño minimo de 2 caracteres
+  ]);
+  EmailControl = new FormControl<string | null>(null,[
+    Validators.required, //Requerido
+    Validators.email //Debe cumplir con las características de un e-mail
+  ]);
+  ClaveControl = new FormControl<string | null>(null,[
+    Validators.required, //Requerido
+  ]);
+  TipoControl = new FormControl<string | null>(null,[
+    Validators.required, //Requerido
+  ]);
 
+
+  usersForm = new FormGroup({
+    nombre: this.NombreControl,
+    apellido1: this.PrimerApellidoControl,
+    apellido2: this.SegundoApellidoControl,
+    telefono: this.TelefonoControl,
+    direccion: this.DireccionControl,
+    email: this.EmailControl,
+    clave: this.ClaveControl,
+    tipo: this.TipoControl
+  });
+
+
+constructor(private dialogRef: MatDialogRef<RegisterComponentComponent>,private store:Store,
+  ){
+    store.select(selectAuthRole).subscribe((userRole) => this.rol = userRole);
+    this.TipoControl.setValue("Cliente");
+  }
+
+
+  onSubmint():void{
+    if(this.usersForm.invalid){
+      alert("Por favor llene todos los campos correctamente");
+    }else{
+      //alert(JSON.stringify(this.usersForm.value));
+      this.dialogRef.close(this.usersForm.value);
+    }
+  }
 }
