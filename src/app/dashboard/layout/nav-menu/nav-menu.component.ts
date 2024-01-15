5f9d2a1f53128d34a4220ded40aecf9ca06f6156
Gothmaur <78580948+Gothmaur@users.noreply.github.com>
2024-01-14 18:05:36 -0600

    optimización de función goTo

--- src/app/dashboard/layout/nav-menu/nav-menu.component.ts
+++ src/app/dashboard/layout/nav-menu/nav-menu.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
+import { Store } from '@ngrx/store';
 import { AuthService } from 'src/app/auth/Services/auth.service';
+import { selectAuthRole } from 'src/app/store/auth/auth.selector';
 
 @Component({
   selector: 'app-nav-menu',
@@ -8,49 +10,23 @@ import { AuthService } from 'src/app/auth/Services/auth.service';
   styleUrls: ['./nav-menu.component.scss']
 })
 export class NavMenuComponent {
-goToInventarios() {
-  this.router.navigate(['dashboard','inventarios'],{
-    //relativeTo: this.activatedRoute,
-  });
-}
-goToPedidos() {
-  this.router.navigate(['dashboard','pedidos'],{
-    //relativeTo: this.activatedRoute,
-  });
-}
 
-  constructor(private router: Router, private authService: AuthService){}
+
+  public rol:string|undefined;
+
+
+  constructor(private router: Router, private authService: AuthService,private store:Store){
+    store.select(selectAuthRole).subscribe((userRole) => this.rol = userRole);
+  }
 
   LogOut():void{
     this.router.navigate(['auth','login'],{});
     this.authService.logOut();
-
   }
 
-  goToUsers():void{
-    this.router.navigate(['dashboard','users'],{
-      //relativeTo: this.activatedRoute,
-    });
-  }
-
-  goToProducts():void{
-    this.router.navigate(['dashboard','products'],{
-      //relativeTo: this.activatedRoute,
-    });
-  }
-
-  goToHome():void{
-    this.router.navigate(['dashboard','home'],{
-      //relativeTo: this.activatedRoute,
-    });
-  }
-
-  
-  goToPerfil():void{
-    this.router.navigate(['dashboard','perfil'],{
-      //relativeTo: this.activatedRoute,
-    });
-  }
+  goTo(ruta:string):void{
+    this.router.navigate(['dashboard',ruta],{})
+  } 
 
 }
 
